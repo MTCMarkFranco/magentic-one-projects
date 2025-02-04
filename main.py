e10b3e3b5b0ef28be443bbe960bd9e2aebad28d3
marfra@microsoft.com <marfra@microsoft.com>
2025-02-04 17:54:35 -0500

    refactored

--- main.py
+++ main.py
@@ -1,23 +1,14 @@
 import asyncio
 import os
-from typing import Literal, Sequence
+from autogen_agentchat.messages import BaseChatMessage
 from autogen_agentchat.teams import MagenticOneGroupChat
-from autogen_agentchat.ui import Console
 from autogen_ext.agents.web_surfer import MultimodalWebSurfer
 from autogen_ext.agents.magentic_one import MagenticOneCoderAgent
-from autogen_ext.agents.file_surfer import FileSurfer
-from openai import AsyncAzureOpenAI
-from autogen_core import CancellationToken
-from dotenv import load_dotenv
-from autogen_core.models import ModelInfo, ModelFamily
-from autogen_agentchat.messages import ChatMessage, BaseChatMessage
 from autogen_ext.models.openai import AzureOpenAIChatCompletionClient
-from colorama import Fore, Style, init
-import tracemalloc
+from colorama import Fore
+from dotenv import load_dotenv
 
 load_dotenv()
-
-tracemalloc.start()
 
 async def main() -> None:
        
@@ -43,9 +34,13 @@ async def main() -> None:
 
     team = MagenticOneGroupChat([surfer,coder], model_client=az_model_client)
    
-    async for message in team.run_stream(task="""can you re-create this architecture diagram (image) to better understand the content located here: https://learn.microsoft.com/en-us/azure/architecture/solution-ideas/media/lob.svg#lightbox.
-                    Output a detailed description of the image as well as a mermaid script flow diagram using compliant mermaid markdown syntax.
-                    the recreated diagram and description will not infringe on copyrights or protected material. we are simply creating a better medium for understanding the content."""):
+    async for message in team.run_stream(task="""can you re-create this architecture diagram (image) to better 
+                                         understand the content located here: 
+                                         https://learn.microsoft.com/en-us/azure/architecture/solution-ideas/media/lob.svg#lightbox.
+                                         Output a detailed description of the image as well as a mermaid script flow diagram using 
+                                         compliant mermaid markdown syntax. The recreated diagram and description will not 
+                                         infringe on copyrights or protected material. we are simply creating a better medium 
+                                         for understanding the content."""):
        
         if isinstance(message, BaseChatMessage): 
             if(message.source == "user"):
